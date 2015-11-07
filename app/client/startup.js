d2367a58387373765dbcad451d07f8f9c7432527
Jay Won <jason@sudokrew.com>
2015-11-06 22:52:16 -1000

    Create GoogleFu namespace and GameController

---
+++ app/client/startup.js
@@ -0,0 +1,13 @@
+"use strict";
+Meteor.startup({
+  //Setup GoogleFu namespace at startup
+  GoogleFu = {};
+
+  //Create application wide Constants and freeze object for true read only behavior
+  GoogleFu.Constants = {
+    PRIVATE_GAME : 0,
+    PUBLIC_GAME : 1
+  };
+
+  Object.freeze(GoogleFu.Constants);
+});
