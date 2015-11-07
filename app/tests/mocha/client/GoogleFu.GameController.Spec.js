117b2aa781f48e7cb3c0cf84fb656ea7e8fb2231
Jay Won <jason@sudokrew.com>
2015-11-07 11:28:44 -1000

    Convert test to ES6 and add passing tests for public/private game create
    - Removed dummy sample test stub
    - Converted syntax to ES6 arrow functions and let declarations
    - Added passing tests for creation of both public/private game creation
    with a Mongo insert stub to test that Mongo insert methods will receive
    proper values.

--- app/tests/mocha/client/GoogleFu.GameController.Spec.js
+++ app/tests/mocha/client/GoogleFu.GameController.Spec.js
@@ -1,26 +1,39 @@
-Games = {};
+var Games = {};
 
 if (!(typeof MochaWeb === 'undefined')){
   MochaWeb.testOnly(function(){
     let expect = chai.expect;
 
-    before(function() {
-
-    });
-
     describe("GoogleFu.GameController", () => {
-      it("should create a new public game with single user", function(){
-        let cursorSimulator = function(err, result){}
+      it("should create a new public game with single user", () => {
+        // let cursorSimulator = (err, result) => {}
 
-        Games.insert = function(obj, done){
+        Games.insert = (obj, done) => {
           expect(obj.title).to.be.equal('Ooogle Octupus');
           expect(obj.type).to.be.equal(1);
           expect(obj.code).to.be.a('null');
           expect(obj.players).to.be.a('array');
+          expect(obj.players).to.have.length(1);
           return cursorSimulator;
         }
 
-        expect(GoogleFu.GameController.createPublicGame()).to.deep.equal(cursorSimulator);
+        GoogleFu.GameController.createPublicGame();
+      });
+
+      it("should create a new private game with single user and generate a unique room code", () => {
+        // let cursorSimulator = (err, result) => {}
+
+        Games.insert = (obj, done) => {
+          expect(obj.title).to.be.equal('Ooogle Octupus');
+          expect(obj.type).to.be.equal(0);
+          expect(obj.code).to.be.a('string');
+          expect(obj.players).to.be.a('array');
+          expect(obj.players).to.have.length(1);
+          return cursorSimulator;
+        }
+
+        // expect(GoogleFu.GameController.createPrivateGame()).to.deep.equal(cursorSimulator);
+        GoogleFu.GameController.createPrivateGame();
       });
     });
   });
