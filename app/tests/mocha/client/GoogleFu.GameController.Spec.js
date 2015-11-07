ebe06cee1e1c7bdfc5601b2cbda5d89e0b41945b
Jay Won <jason@sudokrew.com>
2015-11-07 03:10:03 -1000

    Add unfinished unit test for GoogleFu.GameController

--- app/tests/mocha/client/GoogleFu.GameController.Spec.js
+++ app/tests/mocha/client/GoogleFu.GameController.Spec.js
@@ -2,19 +2,25 @@ Games = {};
 
 if (!(typeof MochaWeb === 'undefined')){
   MochaWeb.testOnly(function(){
+    let expect = chai.expect;
+
     before(function() {
 
     });
 
-    describe("GoogleFu.GameController", function(){
+    describe("GoogleFu.GameController", () => {
       it("should create a new public game with single user", function(){
-        let cursorSimulator;
+        let cursorSimulator = function(err, result){}
 
         Games.insert = function(obj, done){
-          expect(obj.title).toBe('');
+          expect(obj.title).to.be.equal('Ooogle Octupus');
+          expect(obj.type).to.be.equal(1);
+          expect(obj.code).to.be.a('null');
+          expect(obj.players).to.be.a('array');
+          return cursorSimulator;
         }
 
-        expect(GoogleFu.GameController.createPublicGame()).toBe(cursorSimulator);
+        expect(GoogleFu.GameController.createPublicGame()).to.deep.equal(cursorSimulator);
       });
     });
   });
