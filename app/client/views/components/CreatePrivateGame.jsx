b9e91b83749b9998499a9b1dac0eb56e30fd4419
Jay Won <jason@sudokrew.com>
2015-11-08 10:13:27 -1000

    Update Create private game validationto match create public game.

--- app/client/views/components/CreatePrivateGame.jsx
+++ app/client/views/components/CreatePrivateGame.jsx
@@ -1,18 +1,32 @@
 CreatePrivateGame = React.createClass({
-  _createPrivateGame() {
+  getInitialState() {
+    return {
+      warning : null
+    };
+  },
+
+  _createGame() {
     let userName = ReactDOM.findDOMNode(this.refs.userName).value.trim();
+
+    if (!userName) {
+      // display error message
+      this.setState({warning: 'Enter your name!'});
+      return;
+    }
+
     GoogleFu.GameController.createPrivateGame(userName, (err, result) => {
       FlowRouter.go('/lobby/' + result);
     });
   },
 
   render() {
+    let warningTag = (this.state.warning) ? <p className="warning">{ this.state.warning }</p> : '';
+
     return (
-      <div className="create-game">
-        <div className="container">
-          <input type="text" ref="userName" placeholder="What is your name?" />
-          <button className="button button--green" onClick={this._createPrivateGame}>Create Game</button>
-        </div>
+      <div className="create-game container">
+        {warningTag}
+        <input type="text" ref="userName" placeholder="What is your name?" />
+        <button className="button button--green" onClick={this._createGame}>Create Game</button>
       </div>
     )
   }
