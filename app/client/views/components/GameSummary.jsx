dc98308630dd921dca2f0fd060fdb7a5981c343b
Jay Won <jason@sudokrew.com>
2015-11-08 12:18:08 -1000

    Tally final top user score and show winner by name.

--- app/client/views/components/GameSummary.jsx
+++ app/client/views/components/GameSummary.jsx
@@ -15,9 +15,27 @@ GameSummary = React.createClass({
 
   render() {
 
+    let scores = answers.reduce( (userScores, answer) =>{
+      if(!userScores.hasOwnProperty(answer.user)) {
+        userScores[answer.user] = 0;
+      }
+      userScores[answer.user] += answer.points;
+
+      return userScores;
+    }, {});
+
+    let winner = scores.sort((a, b) => {
+      return a.points - b.points;
+    }).reverse()[0];
+
+    let topPlayer = Meteor.users.findOne({_id: winner.user}).profile.name;
+
     return (
       <div className="game-play container">
-        <h1>Game Summary</h1>
+        <h1>Our Winner Is</h1>
+        <div>
+          {topPlayer}
+        </div>
         <button type="button" onClick={this._goHome}>Start Over</button>
       </div>  
     );
