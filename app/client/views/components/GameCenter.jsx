cd9849e6e4d86af185f81ea13f51c2488cb6400e
Emmanuel Pilande <manny@sudokrew.com>
2015-11-08 01:18:22 -1000

    Style round & timing

--- app/client/views/components/GameCenter.jsx
+++ app/client/views/components/GameCenter.jsx
@@ -39,7 +39,7 @@ GameCenter = React.createClass({
 
     return (
       <div className="game-play container">
-        <h1>{ currentRound } <span className="time-remaining">{ timeRemaining }</span></h1>
+        <h3>Round { currentRound } <span className="time-remaining">{ timeRemaining }</span></h3>
         <div className="question-image" style={ questionBGimgStyle }></div>
         <textarea rows="3" ref="playerAnswer" placeholder="What would you Google to get this as your #1 image result?"></textarea>
         <button className="button button--blue" type="button" onClick={this._search}>Search</button>
