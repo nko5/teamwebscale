e40a6fc3c895d71e09a2dd3f1d785eaedb7c4aed
Emmanuel Pilande <manny@sudokrew.com>
2015-11-08 04:39:49 -1000

    Validate username on create public game

--- app/client/views/components/CreatePublicGame.jsx
+++ app/client/views/components/CreatePublicGame.jsx
@@ -1,18 +1,36 @@
 CreatePublicGame = React.createClass({
+  getInitialState() {
+    return {
+      warning : null
+    };
+  },
+
   _createGame() {
     let userName = ReactDOM.findDOMNode(this.refs.userName).value.trim();
+
+    if (!userName) {
+      // display error message
+      this.setState({warning: 'Enter your name!'});
+      return;
+    }
+
     GoogleFu.GameController.createPublicGame(userName, (err, result)=>{
       FlowRouter.go('/lobby/' + result);
     });
   },
 
+  _resetError(event){
+    this.setState({ warning : null });
+  },
+
   render() {
+    let warningTag = (this.state.warning) ? <p className="warning">{ this.state.warning }</p> : '';
+
     return (
-      <div className="create-game">
-        <div className="container">
-          <input type="text" ref="userName" placeholder="What is your name?" />
-          <button className="button button--blue" onClick={this._createGame}>Create Game</button>
-        </div>
+      <div className="create-game container">
+        {warningTag}
+        <input type="text" ref="userName" placeholder="What is your name?" onChange={this._resetError} />
+        <button className="button button--blue" onClick={this._createGame}>Create Game</button>
       </div>
     )
   }
