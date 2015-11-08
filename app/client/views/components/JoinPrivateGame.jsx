beeb6348cdc8c5a7663fe59858d8af80c4550efc
Emmanuel Pilande <manny@sudokrew.com>
2015-11-08 03:23:00 -1000

    Style join private game page

--- app/client/views/components/JoinPrivateGame.jsx
+++ app/client/views/components/JoinPrivateGame.jsx
@@ -5,11 +5,10 @@ JoinPrivateGame = React.createClass({
 
   render() {
     return (
-      <div className="home">
-        <h1>Join Private Game</h1>
+      <div className="join-game container">
         <input type="text" ref="roomCode" placeholder="Room Code"/>
         <input type="text" ref="userName" placeholder="What is your name?" />
-        <button onClick={this._joinGame}>Join Game</button>
+        <button className="button button--green" onClick={this._joinGame}>Join Game</button>
       </div>
     )
   }
