9944fb5f4f5929bc371caeea6755b8aad6ac43d4
Emmanuel Pilande <manny@sudokrew.com>
2015-11-07 17:29:31 -1000

    Draw `X` icon. onClick, go home

--- app/client/views/layouts/Header.jsx
+++ app/client/views/layouts/Header.jsx
@@ -5,10 +5,17 @@ Header = React.createClass({
     }
   },
 
+  _goHome() {
+    FlowRouter.go('/');
+  },
+
   render() {
     return (
       <header>
         {this.state.title}
+        <a href="#" className="close-icon hide-text" onClick={this._goHome}>
+          Close
+        </a>
       </header>
     )
   }
