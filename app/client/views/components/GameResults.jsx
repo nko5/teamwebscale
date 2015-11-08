8338c117cb756a86f6ebaecc3cd40edca7cfd475
Jon Borgonia <jon@gomagames.com>
2015-11-08 03:17:58 -1000

    add player answers to result screen

--- app/client/views/components/GameResults.jsx
+++ app/client/views/components/GameResults.jsx
@@ -20,8 +20,23 @@ GameResults = React.createClass({
     if(this.data.game){
       currentImage = this.data.game.currentImage;
       currentRound = this.data.game.rounds.length;
-      results = this.data.game.answers.map((answer, idx) => {
-        return <PlayerResult key={idx} answer={answer} />;
+      results = this.data.game.players.map( playerId => {
+        return Meteor.users.findOne({ _id : playerId });
+      }).map( player => {
+        let playerAnswer = JSON.parse(JSON.stringify(player)); // clone
+        let playerAnswers = this.data.game.answers.filter( answer => answer.user == playerAnswer._id );
+        if( playerAnswers.length > 0 ){
+          // there's an answer, display it
+
+          playerAnswer.guess = playerAnswers[0].guess;
+          playerAnswer.image = playerAnswers[0].image;
+          playerAnswer.points = playerAnswers[0].points;
+        }else{
+          playerAnswer.guess = 'still thinking...';
+        }
+        return playerAnswer;
+      }).map( (player, idx) => {
+        return <PlayerResult key={idx} player={player} />;
       });
     }
     let questionBGimgStyle = {
@@ -43,22 +58,22 @@ GameResults = React.createClass({
 
 PlayerResult = React.createClass({
   propTypes: {
-    answer: React.PropTypes.object.isRequired
+    player: React.PropTypes.object.isRequired
   },
 
   render() {
     let thumbnailStyle = {
-      backgroundImage : `url('${ this.props.answer.image }')`
+      backgroundImage : `url('${ this.props.player.image }')`
     };
 
     return (
       <li className="player-result">
         <div className="player-result-words">
-          <h4 className="player-result-name">{this.props.answer.user}</h4>
-          <p className="player-answer">{this.props.answer.guess}</p>
+          <h4 className="player-result-name">{this.props.player.profile.name}</h4>
+          <p className="player-answer">{this.props.player.guess}</p>
         </div>
         <div className="player-result-icons">
-          <span className="player-result-status">{this.props.answer.points}</span>
+          <span className="player-result-status">{this.props.player.points}</span>
           <span className="player-result-image-thumbnail" style={thumbnailStyle}></span>
         </div>
       </li>
