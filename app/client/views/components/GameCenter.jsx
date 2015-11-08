ca9533227a29c770d7a46bb05dea4ab60b1db870
Jon Borgonia <jon@gomagames.com>
2015-11-07 22:02:03 -1000

    start GameCenter templating

--- app/client/views/components/GameCenter.jsx
+++ app/client/views/components/GameCenter.jsx
@@ -3,15 +3,25 @@ GameCenter = React.createClass({
 
   getMeteorData() {
     return {
-      games: Games.find({
+      game : Games.find({
         _id: FlowRouter.getParam('id')
       }).fetch()
     }
   },
 
   render() {
+    let timeRemaining = 0;
+    let questionBGimgStyle = {
+      backgroundImage : `url('${ this.data.game.currentImage }')`
+    };
+
     return (
-      <h1>This is a game</h1>
+      <div className="game-play container">
+        <h1>{ this.data.game.round } <span className="time-remaining">{ timeRemaining }</span></h1>
+        <div className="question-image" style={ questionBGimgStyle }></div>
+        <input type="text" name="playerAnswer" />
+        <button type="button">Search</button>
+      </div>
     )
   }
 });
